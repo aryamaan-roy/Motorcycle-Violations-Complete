6eb20a07870959f7096da2516dfd9d4dd551fb0b
Aryamaan Basu Roy <67855588+aryamaan-roy@users.noreply.github.com>
2023-08-09 15:08:33 +0530

    yolov7 and yolo-NAS

---
+++ Training/Models/YOLO-NAS/Bash_scripts/motor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##SBATCH -w gnode076
+#SBATCH -A aryamaan_roy
+#SBATCH -c 10
+##SBATCH --nodes=2
+#SBATCH --gres=gpu:1
+#SBATCH --time=4-00:00:00
+##SBATCH --mail-type=ALL
+##SBATCH --mem-per-cpu=3000
+#SBATCH --output=motor.txt
+
+source activate base
+
+module load u18/cuda/10.2
+module load u18/cudnn/7.6.5-cuda-10.2
+
+export CUDA_VISIBLE_DEVICES=0
+
+cd /home2/aryamaan_roy/Motorcycle-Violations-Complete/Training/YOLO-NAS
+
+python3 train_med_motor.py
+
+echo ----Training Complete----
---
+++ Training/Models/YOLOv7/Bash Scripts/HNH.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+##SBATCH -w gnode076
+#SBATCH -A aryamaan_roy
+#SBATCH -c 10
+##SBATCH --nodes=2
+#SBATCH --gres=gpu:1
+#SBATCH --time=4-00:00:00
+##SBATCH --mail-type=ALL
+##SBATCH --mem-per-cpu=3000
+#SBATCH --output=HNH.txt
+
+source activate base
+
+module load u18/cuda/10.2
+module load u18/cudnn/7.6.5-cuda-10.2
+
+export CUDA_VISIBLE_DEVICES=0
+
+cd /home2/aryamaan_roy/Motorcycle-Violations-Complete/Training/YOLOv7/yolov7
+
+python3 train.py --weights ./../weights/pretrained.pt --data "data/HNH.yaml" --workers 2 --batch-size 4 --cfg cfg/training/yolov7.yaml --name yolov7 --hyp data/hyp.scratch.p5.yaml --epochs 150
+
+echo ----Training Complete----
